7e2c586e334b63b7aac0ecf120afe4193f5c052e
Matthew Feickert <matthew.feickert@cern.ch>
2019-11-30 20:47:52 -0600

    printf debugging

--- tests/test_format_notebook_content.py
+++ tests/test_format_notebook_content.py
@@ -6,12 +6,18 @@ import blackbook
 
 def test_format_notebook_content():
     data_path = pathlib.Path(f"{__file__}").parent / "data"
+    print(f"data_path: {data_path}\n")
     source_notebook_path = data_path / "unformatted" / "spaces.ipynb"
+    print(f"source_notebook_path: {source_notebook_path}\n")
     output_json = blackbook.format_notebook_content(source_notebook_path)
+    print(f"output_json: {output_json}\n")
 
     formatted_notebook_path = data_path / "formatted" / "spaces.ipynb"
+    print(f"formatted_notebook_path: {formatted_notebook_path}\n")
     expected_content = formatted_notebook_path.read_text()
+    print(f"expected_content: {expected_content}\n")
     expected_json = json.loads(expected_content)
+    print(f"expected_json: {expected_json}\n")
     print(
         [
             cell["source"] == expected_cell["source"]
